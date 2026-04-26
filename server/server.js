ce34a5f7cdc9e7260150f61fa883e11914eab7b9
Akiradesign <gdevfullstack@gmail.com>
2026-04-26 12:30:57 +0100

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -43,7 +43,8 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(
   helmet({
-    crossOriginResourcePolicy: false
+    crossOriginResourcePolicy: false,
+    contentSecurityPolicy: false
   })
 );
 app.use(
